515e206dcca2079c3e5300308a3d685cbc8bc4f8
Jaroslaw Gliwinski <jaroslaw.m.gliwinski@gmail.com>
2016-07-23 00:11:07 +0200

    Added missing models & model properties

---
+++ Sources/PokePartyShared/Model/Team.swift
@@ -0,0 +1,20 @@
+//
+//  Team.swift
+//  PokePartyShared
+//
+//  Created by Jaroslaw Gliwinski on 2016-07-22.
+//
+//
+
+import Foundation
+
+
+public enum Team: String {
+    case Valor
+    case Mystic
+    case Instinct
+
+//    public init?(raw: String) {
+//        self.init(rawValue: raw)
+//    }
+}
