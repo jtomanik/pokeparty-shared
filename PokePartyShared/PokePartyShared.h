bc6ee461818960c6053104113cd2d7a68c6b697d
Jaroslaw Gliwinski <jaroslaw.m.gliwinski@gmail.com>
2016-07-22 21:54:23 +0200

    Pokemon model

---
+++ PokePartyShared/PokePartyShared.h
@@ -0,0 +1,19 @@
+//
+//  PokePartyShared.h
+//  PokePartyShared
+//
+//  Created by Jaroslaw Gliwinski on 2016-07-22.
+//  Copyright © 2016 Jaroslaw Gliwinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PokePartyShared.
+FOUNDATION_EXPORT double PokePartySharedVersionNumber;
+
+//! Project version string for PokePartyShared.
+FOUNDATION_EXPORT const unsigned char PokePartySharedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PokePartyShared/PublicHeader.h>
+
+
