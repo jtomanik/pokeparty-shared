fefc7ae681f12df57d1927e815296223cab382cd
Jakub Tomanik <j.tomanik@gmail.com>
2016-07-22 19:50:04 +0200

    Linux dependency

--- Package.swift
+++ Package.swift
@@ -1,5 +1,9 @@
 import PackageDescription
 
-let package = Package(
+var package = Package(
     name: "PokePartyShared"
 )
+
+#if os(Linux)
+  package.dependencies.append([.Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 9)])
+#endif
